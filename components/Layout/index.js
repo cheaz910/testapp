45d420a30ac878efe4637008e98232a2423bb232
Ilnur <cheaz910@mail.ru>
2020-11-24 16:33:39 +0500

    1212

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -13,11 +13,13 @@ function App({
   const metaUrl = url || ''
   return (
     <div className="App">
-      <meta property="og:title" content={metaTitle} />
-      <meta name="description" content={metaDescription} />
-      <meta name="og:description" content={metaDescription} />
-      <meta property="og:locale" content="ru_RU" />
-      <meta property="og:locale:alternate" content="en_US" />
+      <Head>
+        <meta property="og:title" content={metaTitle} />
+        <meta name="description" content={metaDescription} />
+        <meta name="og:description" content={metaDescription} />
+        <meta property="og:locale" content="ru_RU" />
+        <meta property="og:locale:alternate" content="en_US" />
+      </Head>
       {children}
     </div>
   );
