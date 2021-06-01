91e4358fe33fd65417678e70f728c6b429e70b93
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-01 13:11:37 +0000

    Deploy cheaz910/testapp to github.com/cheaz910/testapp.git:gh-pages

---
+++ _next/static/8HHZUng9x21FP9oBduYSw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-d95b58118989105ca3b7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f9f21b98f1bf926afb8a.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
